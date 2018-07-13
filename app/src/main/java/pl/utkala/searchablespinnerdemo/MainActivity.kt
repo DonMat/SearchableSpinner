07fa97b1acdae412089b67d6b459792f8e1bbde7
Mateusz Utkała <mateusz@utkala.pl>
2018-07-14 00:55:20 +0200

    Improve demo app

--- app/src/main/java/pl/utkala/searchablespinnerdemo/MainActivity.kt
+++ app/src/main/java/pl/utkala/searchablespinnerdemo/MainActivity.kt
@@ -16,13 +16,18 @@
 
 package pl.utkala.searchablespinnerdemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val users = listOf("Veniam penatibus", "reprehenderit", "fusce", "Ullamcorper lacinia", "Etiam dis")
+        searchableSpinner.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, users)
     }
 }
